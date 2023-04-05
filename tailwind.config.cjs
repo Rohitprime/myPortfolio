b1d9173f93357f895d3296f9dcacef04e322db57
Rohit Kumar <rohitprime0@gamil.com>
2023-04-05 23:31:04 +0530

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      animation: {
+        slideup: 'slideup 1s ease-in-out',
+        slidedown: 'slidedown 1s ease-in-out',
+        slideleft: 'slideleft 1s ease-in-out',
+        slideright: 'slideright 1s ease-in-out',
+        wave: 'wave 1.2s linear infinite',
+        slowfade: 'slowfade 2.2s ease-in-out',
+      },
+      keyframes: {
+        slowfade: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideup: {
+          from: { opacity: 1, transform: 'none' },
+          to: { opacity: 0, transform: 'translateY(-100vh)' },
+        },
+        slideupward: {
+          from: { opacity: 1, transform: 'none' },
+          to: { opacity: 0, transform: 'translateY(-100vh)' },
+        },
+        slidedown: {
+          from: { opacity: 0, transform: 'translateY(-100vh)' },
+          to: { opacity: 1, transform: 'none' },
+        },
+        slideleft: {
+          from: { opacity: 0, transform: 'translateX(-100vw)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideright: {
+          from: { opacity: 0, transform: 'translateX(20px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        wave: {
+          '0%': { transform: 'scale(0)' },
+          '50%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(0)' },
+        },
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
